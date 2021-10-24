ba9dee8f10d483e66f26d6f9afa47986dc8769f0
DenisTelicin <telicinda@gmail.com>
2021-10-24 07:34:07 +0300

    rendered fine

--- client/src/components/Dream.tsx
+++ client/src/components/Dream.tsx
@@ -1,22 +1,31 @@
-import React, {FunctionComponent} from 'react';
-import {UpdateDream, DeleteDream} from "./index";
+import React, { FunctionComponent } from 'react';
+import { UpdateDream, DeleteDream } from './index';
 import defaultPicture from '../assets/images/flat.jpg';
-import { Card, Grid, makeStyles, CardMedia, CardHeader, CardContent, Icon, Paper } from "@material-ui/core";
+import {
+  Card,
+  Grid,
+  makeStyles,
+  CardMedia,
+  CardHeader,
+  CardContent,
+  Icon,
+  Paper,
+} from '@material-ui/core';
 
 const useStyles = makeStyles({
   wrapper: {
     minHeight: '350px',
   },
   content: {
-    paddingTop: 0
+    paddingTop: 0,
   },
   mediaWrapper: {
-    position: "relative"
+    position: 'relative',
   },
   media: {
     width: '100%',
     height: '200px',
-    borderRadius: '3px'
+    borderRadius: '3px',
   },
   rating: {
     position: 'absolute',
@@ -28,24 +37,27 @@ const useStyles = makeStyles({
     padding: '2px 5px',
     borderRadius: '3px',
     fontWeight: 600,
-    color: '#af9723'
+    color: '#af9723',
   },
   icon: {
     marginLeft: '10px',
-    color: '#af9723'
+    color: '#af9723',
   },
   date: {
-    paddingRight: '16px'
+    paddingRight: '16px',
   },
   footer: {
     paddingTop: '16px',
     paddingRight: '16px',
     paddingBottom: '16px',
-  }
+  },
 });
 
 const Dream: FunctionComponent<Props> = (props) => {
-  const {dream, dream: {_id, image = defaultPicture}} = props;
+  const {
+    dream,
+    dream: { _id, image = defaultPicture },
+  } = props;
   const classes = useStyles();
 
   if (!_id) return null;
@@ -54,25 +66,27 @@ const Dream: FunctionComponent<Props> = (props) => {
     <Grid item xs={6}>
       <Paper elevation={2}>
         <Card className={classes.wrapper}>
-          <Grid container justify='space-between' alignItems='center'>
-            <CardHeader title={dream.name}/>
+          <Grid container justify="space-between" alignItems="center">
+            <CardHeader title={dream.name} />
             <span className={classes.date}>{dream.time}</span>
           </Grid>
 
           <CardContent className={classes.content}>
             <div className={classes.mediaWrapper}>
-              <CardMedia className={classes.media}
-                         image={image}
-                         title={dream.name}/>
+              <CardMedia
+                className={classes.media}
+                image={image}
+                title={dream.name}
+              />
               <div className={classes.rating}>
                 {dream.rating}
                 <Icon className={classes.icon}>star</Icon>
               </div>
             </div>
           </CardContent>
-          <Grid container justify='flex-end' className={classes.footer}>
-            <UpdateDream id={_id}/>
-            <DeleteDream id={_id}/>
+          <Grid container justify="flex-end" className={classes.footer}>
+            <UpdateDream id={_id} />
+            <DeleteDream id={_id} />
           </Grid>
         </Card>
       </Paper>
@@ -82,9 +96,8 @@ const Dream: FunctionComponent<Props> = (props) => {
 
 export default Dream;
 
-
 interface OwnProps {
-  dream: TDream
+  dream: TDream;
 }
 
 export type TDream = {
@@ -93,6 +106,6 @@ export type TDream = {
   rating: number | null;
   time: string;
   image?: string;
-}
+};
 
 type Props = OwnProps;
