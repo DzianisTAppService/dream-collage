564ff85785c4d26e1028d3c9cfa34bce26bce795
DenisTelicin <telicinda@gmail.com>
2022-04-20 13:45:16 +0200

    adding UploadingField component in UI

--- client/src/common/DreamForm/UploadField/UploadField.tsx
+++ client/src/common/DreamForm/UploadField/UploadField.tsx
@@ -1,10 +1,14 @@
 // https://www.npmjs.com/package/react-images-uploading
 import React from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
-import { Box, Button, FormControl, FormHelperText } from '@mui/material';
-import UploadFileIcon from '@mui/icons-material/UploadFile';
-
-import { StyledInput } from './UploadField.styles';
+import ImageUploading from 'react-images-uploading';
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  FormHelperText,
+} from '@mui/material';
 
 const UploadField = () => {
   const {
@@ -27,18 +31,66 @@ const UploadField = () => {
             border="1px solid #0000003b"
             borderRadius={1}
           >
-            <label htmlFor="image">
-              <StyledInput
-                id="image"
-                type="file"
-                onChange={onChange}
-                value={value}
-              />
-              <Button variant="contained" component="span">
-                <UploadFileIcon />
-              </Button>
-            </label>
-            <FormHelperText>{value}</FormHelperText>
+            <ImageUploading
+              multiple
+              value={value}
+              onChange={onChange}
+              maxNumber={1}
+            >
+              {({
+                imageList,
+                onImageUpload,
+                onImageUpdate,
+                onImageRemove,
+                isDragging,
+                dragProps,
+              }: any) => (
+                <Box flexGrow={1}>
+                  {!imageList.length && (
+                    <Button
+                      variant="contained"
+                      style={isDragging ? { color: 'red' } : undefined}
+                      onClick={onImageUpload}
+                      {...dragProps}
+                    >
+                      Click or Drop here
+                    </Button>
+                  )}
+                  {imageList.map((image: any, index: any) => (
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                      key={index}
+                    >
+                      <img src={image.dataURL} alt="" width="100%" />
+
+                      <Box my={2} width="100%">
+                        <Divider />
+                      </Box>
+
+                      <Box display="flex">
+                        <Button
+                          variant="outlined"
+                          onClick={() => onImageUpdate(index)}
+                        >
+                          Update
+                        </Button>
+
+                        <Box ml={3}>
+                          <Button
+                            variant="outlined"
+                            onClick={() => onImageRemove(index)}
+                          >
+                            Remove
+                          </Button>
+                        </Box>
+                      </Box>
+                    </Box>
+                  ))}
+                </Box>
+              )}
+            </ImageUploading>
           </Box>
           <FormHelperText error>{errors.image?.message}</FormHelperText>
         </FormControl>
